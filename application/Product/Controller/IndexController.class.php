f907716284be2463054986a7f76b70a7376c2288
xuewueagle <1223696476@qq.com>
2017-05-03 18:44:20 +0800

    add module

---
+++ application/Product/Controller/IndexController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Product\Controller;
+use Common\Controller\AdminbaseController;
+
+class IndexController extends AdminbaseController {
+    public function index(){
+    	
+    	$this->display();
+    }
+}
---
+++ data/lang/Product/Lang/zh-cn/admin_menu.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  'PRODUCT_INDEX_ADD' => '添加商品',
+  'PRODUCT_INDEX_DEL' => '删除商品',
+  'PRODUCT_INDEX_INDEX' => '商品管理',
+);
