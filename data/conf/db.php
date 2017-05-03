e2cf15e3b9a1589eb9511dde9681f505a0e1564b
xuewueagle <1223696476@qq.com>
2017-05-03 17:51:36 +0800

    commit gitignore file

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '114.115.212.193',
+    'DB_NAME' => 'shop_share',
+    'DB_USER' => 'root',
+    'DB_PWD' => '123456',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'ss_',
+    //密钥
+    "AUTHCODE" => 'RnL9mLvbcgt5mlbpIu',
+    //cookies
+    "COOKIE_PREFIX" => 'XasjO3_',
+);
